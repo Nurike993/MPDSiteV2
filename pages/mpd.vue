a5baf15572fbaa9b80805e82ffb5a602e9596e11
virg1n <113487825+virg1n@users.noreply.github.com>
2024-05-10 18:49:17 +0500

    mpd

--- pages/mpd.vue
+++ pages/mpd.vue
@@ -1,3 +1,32 @@
 <template>
-    
+    <section class="box-intro">
+            <div class="table-cell">
+                <h1 class="box-headline letters rotate-2">
+                    <span class="box-words-wrapper">
+                        <b class="is-visible">MPD.</b>
+                        <b>knowledge.</b>
+                        <b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CODE.</b>
+                    </span>
+                </h1>
+                <h5>The best IT club in FizMat</h5>
+            </div>
+    </section>
+    <hr/>
+    <div class="container main-container clearfix"> 
+        <div class="col-md-6">
+            <h3 class="uppercase">About US</h3>
+            <h5>Клуб, где все можно сделать своими руками</h5>
+            <div class="h-30"></div>
+            <p class="contact-text">Мы хотим отойти от стандартных основ олимпиадного программирования и давать основу для продвижения в друге сферы такие как создание сайтов, серверные приложения, парсинг, мобильные приложение и тд
+                Этот клуб разовьет желание учеников создавать различные проекты объединяться в группы. Мы хотим пропагандировать и давать базу для людей которые хотят начать програмиировать, но не могут потому-что они не понимают с чего начать обучение, мы будем давать статьи и помогать развиться учащемуся для достижение своей цели.</p>
+        </div>
+        <div class="col-md-6">
+        </div>
+    </div>
+    <hr/>
+    <div class="container main-container">
+        <div class="col-md-4">
+        </div>
+
+    </div>
 </template>
